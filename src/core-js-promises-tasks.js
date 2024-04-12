a76f34258ec17019ecbaca5104e4535cc4e21f1c
olenaweb <olena.nevzorova@gmail.com>
2024-04-12 23:58:31 +0300

    5,6

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -125,10 +125,13 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises)
+    .then((results) => results)
+    .catch((error) => {
+      throw error;
+    });
 }
-
 /**
  * Processes an array of promises and returns a promise that resolves with an array of their results.
  * Each element in the returned array corresponds to the resolved value of the promise at the same index in the input array.
@@ -141,10 +144,12 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+// function getAllResult(/* promises */) {
+//   throw new Error('Not implemented');
+// }
+function getAllResult(promises) {
+  return Promise.all(promises.map((promise) => promise.catch(() => null)));
 }
-
 /**
  * Takes an array of promises and processes them sequentially, concatenating each resolved value into a single string.
  * The resolution order is determined by the order of the promises in the array, not by their resolution time.
